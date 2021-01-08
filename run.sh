8e60fdea3efc3acdef444396774d547cf04ef1cc
Katie Cordes <gillis.katharine@gmail.com>
2021-01-07 20:43:13 -0700

    Updated to be upgrade if already installed

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-choco.exe install microsoft-windows-terminal -y
+choco.exe upgrade microsoft-windows-terminal -y
 
 PKG_PATH=$2
 WIN_TERMINAL_PATH=$(powershell.exe -Command "echo \$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json")
